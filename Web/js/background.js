1413a5faa13e1be57c339638c9e1e7a54fc2c463
shijuvar <shiju.varghese@outlook.com>
2014-04-23 11:29:53 +0530

    Code Cleanup

--- Web/js/background.js
+++
@@ -1,8 +0,0 @@
-﻿chrome.app.runtime.onLaunched.addListener(function () {
-    chrome.app.window.create('index.html', {
-        'bounds': {
-            'width': 800,
-            'height': 500
-        }
-    });
-});
